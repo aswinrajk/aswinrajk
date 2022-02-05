f9d87531bf9e19a9228a4fb0cb8384b97ecbe58f
aswinraj <aswinrajkorkandy@gmail.com>
2022-02-05 12:58:26 +0530

    convertion done

--- App.js
+++ App.js
@@ -1,20 +1,114 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert,TextInput} from 'react-native';
+
+
+
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
+const App = () => {
+
+const [celsius, setcel] = useState('');
+const [faren,setfar] =useState('');
+const [kelvin,setkelv] =useState('');
+const [mile, ml] =useState('');
+
+var onchange=(show)=>{
+  setcel( (show*1609.344).toString());
+ 
+}
+
+
+  return( 
+    
+  <SafeAreaView style={styles.container}>
+    
+       
+    <View>
+   
+    
+     <Text style={styles.title}>
+  celsius
+ </Text> 
+ 
+
+      <TextInput
+       placeholder="celsius"
+       onChangeText={onchange}      
+      />
+      <Separator />
+      <Text style={styles.title1}>
+        faren
+      </Text>
+      
+      
+      <TextInput
+       placeholder="faran"
+       value={mile}
+       onChangeText={onchange}
+
+      
+
+      />
+      
+     
+      
     </View>
+    <Separator />
+    
+    <View>
+     
+      <Button
+        title="CONVERT"
+        color="#f194ff"
+        onPress={() => alert(celsius)}
+      />
+     
+
+    </View>
+   
+ 
+      
+    
+  </SafeAreaView>
   );
 }
+ 
 
 const styles = StyleSheet.create({
   container: {
+     
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems:'center',
+    marginHorizontal: 20,
+    
   },
+  
+  
+  space: {
+    
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 15,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+ 
+    
+    
+  
+  
+  
 });
+
+
+
+export default App;
